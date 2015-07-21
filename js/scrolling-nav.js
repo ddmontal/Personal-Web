954a08013e30746e4daf8b6665541dbca3fcfd24
ddmontal <ddmontal@gmail.com>
2015-07-21 17:10:47 +0200

    Changed color palette

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -14,6 +14,7 @@ $(function() {
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
+        
         event.preventDefault();
     });
 });
